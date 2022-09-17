03ad7e2c713b6d04c53eb3c1d183b550f4f79a52
Liuver C. Espinosa <liuver.carrera.espinosa@gmail.com>
2022-09-17 17:50:33 -0400

    Next first

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The rest of the Cypress config options go here...
+  projectId: "x651dq",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
